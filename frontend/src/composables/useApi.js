9dc8aa47025d533fe8c2e2fbf67b440a286e9d59
Prit <109797484+Prit-WD-2-3@users.noreply.github.com>
2026-04-26 17:38:13 +0530

    Implement API client with Axios and interceptors

---
+++ frontend/src/composables/useApi.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import { useAuthStore } from '../stores/auth'
+import router from '../router'
+
+const api = axios.create({
+  baseURL: '/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+api.interceptors.request.use((config) => {
+  const authStore = useAuthStore()
+  if (authStore.token) {
+    config.headers.Authorization = `Bearer ${authStore.token}`
+  }
+  return config
+})
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      const authStore = useAuthStore()
+      authStore.logout()
+      router.push('/login')
+    }
+    return Promise.reject(error)
+  }
+)
+
+export function useApi() {
+  return api
+}
+
+export default api
